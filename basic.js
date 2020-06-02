6b3d26f1996ea39dc67f8a377c3070139df6bf19
Zaloz64 <57728513+Zaloz64@users.noreply.github.com>
2020-06-03 00:27:04 +0200

    done

--- basic.js
+++ basic.js
@@ -7,20 +7,10 @@ function myFunction() {
     }
 }
 
-function functionBig() {
-
-  console.log('do you work');
-    // // Get the expanded image
-    // var expandImg = document.getElementById("expandedImg");
-    // // Get the image text
-    // var imgText = document.getElementById("imgtext");
-    // // Use the same src in the expanded image as the image being clicked on from the grid
-    // expandImg.src = imgs.src;
-    // // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    // imgText.innerHTML = imgs.alt;
-    // // Show the container element (hidden with CSS)
-    // expandImg.parentElement.style.display = "block";
-  }
+function functionBig(img) {
+  var expandImg = document.getElementById("expandedImg");
+  expandImg.src = img.src;
+}
 
   tns({ 
   "items": 2,
